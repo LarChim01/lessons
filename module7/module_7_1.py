54ea79f07582135d96751803ff409e4301825685
LarChim01 <larchim@yandex.ru>
2024-09-23 19:54:58 +0300

    Update module_7_1.py

--- module7/module_7_1.py
+++ module7/module_7_1.py
@@ -5,6 +5,7 @@ class Product:
         self.name = name
         self.weight = weight
         self.category = category
+        
     def __str__(self):
         return (f"{self.name}, {self.weight}, {self.category}\n")
 
