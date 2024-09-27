b092aa2ceb8f329bc1a726dcd7f1a1c7f5b7426f
LarChim01 <larchim@yandex.ru>
2024-09-27 15:48:05 +0300

    Update module_6_hard.py

--- module6/module_6_hard.py
+++ module6/module_6_hard.py
@@ -1,63 +1,48 @@
 #Дополнительное практическое задание по модулю: "Наследование классов."
 import math
 
-pi = math.pi  # число пи
+PI = math.pi  # число пи
 
 
 class Figure:
     sides_count = 0
 
     def __init__(self, color, *sides):
-        self.__color = color
-        self.set_sides_f(*sides)
-
-    def set_color(self, r, g, b):
-        if self.__is_valid_color(r) and self.__is_valid_color(g) and self.__is_valid_color(b):
-            self.__color = (r, g, b)
-
-    def __is_valid_color(self, c1):
-        if c1 < 0 or c1 > 255:
-            return False
-        return True
-
-    def __is_valid_sides(self, sid):
-        if isinstance(sid, int):
-
-            return True
+        color = list(color)
+        self.__color = [*color] if self.__is_valid_color(*color) else [0, 0, 0]
+        if  len(sides)== 1 :
+            self.__sides = list(sides * self.sides_count)
         else:
-            return False
+            self.__sides = list([*sides] if len(sides) == self.sides_count else [1] * self.sides_count)
 
-    def set_sides(self, *sides):
-        if len(sides) != self.sides_count:
-            return False
-        for i in range(0, len(sides)):
-            if not self.__is_valid_sides(i):
+    def set_color(self, *color):
+        self.__color = [*color] if self.__is_valid_color(*color)  else self.__color
+
+    def __is_valid_color(self, *color):
+        for c1 in color:
+            if not isinstance(c1,int) or c1 >255 or c1< 0:
                 return False
 
-        for i in range(0, len(sides)):
-            self.__sides[i] = sides[i]
+        return True
 
-    def set_sides_f(self, *sides):
-        sids = []
-        for item in sides:
-            sids.append(item)
+    def __is_valid_sides(self, *sides):
+        for sid in sides:
+            if not isinstance(sid, int):
+                return False
 
-        if len(sids) == 1:  # если одна длина, то все стороны такие же
+        return True
 
-            sids = self.set_sides_1(sids[0])
-        elif len(sids) != self.sides_count:
-            sids = self.set_sides_1(1)
+    def set_sides(self, *sides):
+        if self.__is_valid_sides(*sides):
+            self.__sides = list([*sides] if len(sides) == self.sides_count else self.__sides)
 
-        self.__sides = sids
+
+
 
     def get_sides(self):
         return self.__sides
 
-    def set_sides_1(self, dl):  # заполнение параметра
-        sides = []
-        for i in range(0, self.sides_count):
-            sides.append(dl)
-        return sides
+
 
     def get_color(self):
         return self.__color
@@ -78,12 +63,10 @@ class Circle(Figure):
 
     def __init__(self, color, *sides):
         Figure.__init__(self, color, *sides)
-        dl = self.get_sides()
-
-        self._radius = dl[0] / (2 * pi)
+        self.__radius = self.get_sides()[0] / (2 * PI)
 
     def get_square(self):
-        return self._radius ** 2 * pi
+        return self.__radius ** 2 * PI
 
 
 class Triangle(Figure):
@@ -109,12 +92,13 @@ class Cube(Figure):
         '''
         объём куба
         '''
-        sid = self.get_sides()
-        return sid[0] ** 3
+
+        return self.get_sides()[0] ** 3
+class Sixter(Figure):
+    sides_count = 6
 
 
 if __name__ == "__main__":
-    # Код    для    проверки:
     circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
     cube1 = Cube((222, 35, 130), 6)
 
@@ -135,23 +119,3 @@ if __name__ == "__main__":
 
     # Проверка объёма (куба):
     print(cube1.get_volume())
-
-    ###
-    # круг
-    print("------------")
-    print("круг")
-    circ1 = Circle((15, 200, 100), 5)
-    print(f'"Периметр -"{len(circ1)}')
-    print(f'"Площадь -"{circ1.get_square()}')
-
-    # треугольник
-    print("треугольник")
-    tr1 = Triangle((15, 200, 100), 5, 3, 4)
-    print(f'"Периметр -"{len(tr1)}')
-    print(f'"Площадь -"{tr1.get_square()}')
-
-    # куб
-    print("куб")
-    cube1 = Cube((222, 35, 130), 6)
-    print(f'"Сумма длин рёбер -"{len(cube1)}')
-    print(f'"Объём -"{cube1.get_volume()}')
