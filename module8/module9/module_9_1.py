14645657b1a8b0087f1514f14d62688fff6bbad6
LarChim01 <larchim@yandex.ru>
2024-09-28 22:00:04 +0300

    Add files via upload

---
+++ module8/module9/module_9_1.py
@@ -0,0 +1,12 @@
+#Домашнее задание по теме "Введение в функциональное программирование"
+
+def apply_all_func(int_list, *functions):
+    results = {}
+    for fun in functions:
+        results[fun.__name__] = fun(int_list)
+    return results
+
+
+if __name__ == "__main__":
+    print(apply_all_func([6, 20, 15, 9], max, min))
+    print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
