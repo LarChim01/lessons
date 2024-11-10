b0828d950a863afc5551edf368c0ebde29c3de94
LarChim01 <larchim@yandex.ru>
2024-11-10 15:27:44 +0300

    Update crud_functions.py

--- module_14/crud_functions.py
+++ module_14/crud_functions.py
@@ -5,7 +5,7 @@ cursor = connection.cursor()
 def initiate_db():
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products(
-    id INTEGER PRYMARY KEY,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT,
     price 
